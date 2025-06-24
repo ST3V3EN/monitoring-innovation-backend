b9ee7b8212c4723dbf6e78d464698da52e898038
BrayanStevenMorenoG <brasmoreno5@soy.sena.edu.co>
2025-06-23 19:52:53 -0500

    chore: enable CORS to allow frontend connections

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 from app.controller import car
-# from app.db.database import Base, engine
-from app.models import car as car_model
-
-# Base.metadata.create_all(bind=engine)
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Monitoring Innovation API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(car.router)
